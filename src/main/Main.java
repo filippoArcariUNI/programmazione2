088ed9a634eb19f79fa4df73544d85c9e01b0ef6
filippoArcariUNI <filippo.arcari@studenti.unitn.it>
2024-03-16 22:50:32 +0100

    Revert "fill The map whit random blocks"
    This reverts commit 86a157b62840f9b3bf58dee533288095fe3b5058.

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,17 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Map myMap=new Map();
-        for (int i = 0; i < myMap.dimZ; i++) {
-            System.out.print("Enter row: ");
-            Scanner myObj = new Scanner(System.in);
-            int row = myObj.nextInt();
-
-            System.out.print("Enter column: ");
-            int col = myObj.nextInt();
-
-            System.out.println("Changing: "+row+" - "+col);
-            myMap.change_cell(row,col);
-        }
+        myMap.randomMap();
         myMap.display_on_out();
+
+
+
     }
 }
